8bf6815357d2f47cf52483cbcb2be3ff72943b73
healym3 <healym3@gator.uhd.edu>
2022-02-11 11:28:25 -0600

    Add ability to parse JSON OBject and JSON Array

--- app/src/main/java/com/example/parsedata/MainActivity.java
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.parsedata;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.android.volley.*;
@@ -12,10 +14,12 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     String apiUrl = "https://jsonplaceholder.typicode.com/todos";
+    String getApiUrl ="https://jsonplaceholder.typicode.com/todos/1";
     RequestQueue queue;
 
     @Override
@@ -32,13 +36,52 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //       );
         queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = getJsonObjectRequest();
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (Request.Method.GET, apiUrl, null, new Response.Listener<JSONArray>() {
+        JsonArrayRequest jsonArrayRequest = getJsonArrayRequest(textView);
+
+        queue.add(jsonObjectRequest);
+// Access the RequestQueue through your singleton class.
+        //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        queue.add(jsonArrayRequest);
+        //getString(textView, queue);
+    }
+
+    @NonNull
+    private JsonArrayRequest getJsonArrayRequest(TextView textView) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                response -> {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            String title = jsonObject.getString("title");
+                            textView.setText(textView.getText() + title + "\n");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    //textView.setText("Response: " + response.toString())
+                },
+                error -> {
+                    // TODO: Handle error
+                    Log.d("JSON", "onCreate: Failed");
+                });
+        return jsonArrayRequest;
+    }
+
+    @NonNull
+    private JsonObjectRequest getJsonObjectRequest() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, getApiUrl, null, new Response.Listener<JSONObject>() {
 
                     @Override
-                    public void onResponse(JSONArray response) {
-                        textView.setText("Response: " + response.toString());
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("jsonObj", "onCreate: " + response.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        //textView.setText("Response: " + response.toString());
                     }
                 }, new Response.ErrorListener() {
 
@@ -48,11 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
-
-// Access the RequestQueue through your singleton class.
-        //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-        queue.add(jsonArrayRequest);
-        //getString(textView, queue);
+        return jsonObjectRequest;
     }
 
     private void getString(TextView textView, RequestQueue queue) {
